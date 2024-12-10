ac4c354960ec4360851d8e98deb89f94da2cbc49
crisis-argha <namespace.arg@gmail.com>
2024-12-10 23:13:02 +0530

    stopWatch UI

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,16 @@ class HomeApp extends StatefulWidget {
 class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF1C2757),
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C2757),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Center(
+              const Center(
                 child: Text(
                   "StopWatch App",
                   style: TextStyle(
@@ -43,6 +43,66 @@ class _HomeAppState extends State<HomeApp> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Center(
+                child: Text(
+                  "00:00:00",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 82.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                height: 400.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF323F68),
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                      child: const Text(
+                        'Start',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    icon: const Icon(Icons.flag),
+                  ),
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      fillColor: Colors.blue,
+                      shape: const StadiumBorder(),
+                      child: const Text(
+                        'Reset',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ],
           ),
