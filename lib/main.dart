4a542477fa2d37e0c98103b75280c48cb67f884b
crisis-argha <namespace.arg@gmail.com>
2024-12-11 10:53:21 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeApp(),
     );
   }
@@ -66,7 +66,7 @@ class _HomeAppState extends State<HomeApp> {
   void start() {
     started = true;
     timer = Timer.periodic(
-      Duration(seconds: 1),
+      const Duration(seconds: 1),
       (timer) {
         int localSeconds = seconds + 1;
         int localMinutes = minutes;
@@ -95,7 +95,7 @@ class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF1C2757),
+      backgroundColor: const Color(0xFF1C2757),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -118,8 +118,8 @@ class _HomeAppState extends State<HomeApp> {
               ),
               Center(
                 child: Text(
-                  "00:00:00",
-                  style: TextStyle(
+                  "$digitHours:$digitMinutes:$digitSeconds",
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 82.0,
                     fontWeight: FontWeight.w600,
@@ -132,6 +132,41 @@ class _HomeAppState extends State<HomeApp> {
                   color: const Color(0xFF323F68),
                   borderRadius: BorderRadius.circular(16.0),
                 ),
+                //list builder
+                child: Scrollbar(
+                  thumbVisibility: true,
+                  radius: const Radius.circular(16.0),
+                  child: ListView.builder(
+                    itemCount: laps.length,
+                    itemBuilder: (
+                      context,
+                      index,
+                    ) {
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Lap no. ${index + 1}",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text(
+                              "${laps[index]}",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
               const SizedBox(
                 height: 20.0,
@@ -141,13 +176,15 @@ class _HomeAppState extends State<HomeApp> {
                 children: [
                   Expanded(
                     child: RawMaterialButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        (!started) ? start() : stop();
+                      },
                       shape: const StadiumBorder(
                         side: BorderSide(color: Colors.blue),
                       ),
                       child: Text(
-                        'Start',
-                        style: TextStyle(
+                        (!started) ? "start" : "pause",
+                        style: const TextStyle(
                           color: Colors.white,
                         ),
                       ),
@@ -155,15 +192,20 @@ class _HomeAppState extends State<HomeApp> {
                   ),
                   IconButton(
                     color: Colors.white,
-                    onPressed: () {},
-                    icon: Icon(Icons.flag),
+                    onPressed: () {
+                      addLaps();
+                    },
+                    icon: const Icon(Icons.flag),
                   ),
                   Expanded(
                     child: RawMaterialButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        reset();
+                        laps.clear();
+                      },
                       fillColor: Colors.blue,
                       shape: const StadiumBorder(),
-                      child: Text(
+                      child: const Text(
                         'Reset',
                         style: TextStyle(
                           color: Colors.white,
